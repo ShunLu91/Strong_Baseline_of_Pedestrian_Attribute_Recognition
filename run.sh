5c4d5138c555ed05e54818cf66a7b5cd7e200fea
lushun_mac <470651748@qq.com>
2020-06-23 23:49:56 +0800

    resnet101/152 --train_epoch 60

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #CUDA_VISIBLE_DEVICES=0,1,2,3 nohup python -u train.py  > logdir/a0.log  2>&1 &
 #CUDA_VISIBLE_DEVICES=0,1 nohup python -u train.py --model resnet18 > logdir/a1.log  2>&1 &
 #CUDA_VISIBLE_DEVICES=2,3 nohup python -u train.py --model resnet34 > logdir/a2.log  2>&1 &
-CUDA_VISIBLE_DEVICES=0,1 nohup python -u train.py --model resnet101 > logdir/a3.log  2>&1 &
-CUDA_VISIBLE_DEVICES=2,3 nohup python -u train.py --model resnet152 > logdir/a4.log  2>&1 &
+#CUDA_VISIBLE_DEVICES=0,1 nohup python -u train.py --model resnet101 > logdir/a3.log  2>&1 &
+#CUDA_VISIBLE_DEVICES=2,3 nohup python -u train.py --model resnet152 > logdir/a4.log  2>&1 &
+CUDA_VISIBLE_DEVICES=0,1 nohup python -u train.py --model resnet101 --train_epoch 60 > logdir/a5.log  2>&1 &
+CUDA_VISIBLE_DEVICES=2,3 nohup python -u train.py --model resnet152 --train_epoch 60 > logdir/a6.log  2>&1 &
 
-tail -f logdir/a4.log
+tail -f logdir/a6.log
